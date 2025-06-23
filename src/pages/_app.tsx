ed5b5afc4c499aed2e27bfa0ab7f557509bfbffb
Mikayel <mikael.bejanyan@gmail.com>
2025-06-23 18:29:29 +0400

    Minor chagne in frontend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import type { AppProps } from 'next/app';
+import Head from "next/head";
 
 import '../styles/globals.css';
 
 const TestTaskApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default TestTaskApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,6 @@ const Document = () => {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <title>Dashboard</title>
       </Head>
       <body>
         <Main />
